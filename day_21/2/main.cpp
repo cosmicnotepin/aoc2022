0b2b5b825ebfbba915ce0a9d63d3b8be3d4e5d41
dmad <dmad@t14g3.fritz.box>
2022-12-21 21:43:57 +0100

    day_21/2 with amazing grace

--- day_21/2/main.cpp
+++ day_21/2/main.cpp
@@ -11,7 +11,7 @@
 #include <list>
 #include <string>
 #include <memory>
-#include <set>
+#include <unordered_set>
 #include <functional>
 #include <limits>
 #include <algorithm>
@@ -19,12 +19,13 @@
 
 typedef long unsigned int luint;
 typedef long long int llint;
+typedef std::string ss;
 template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
 
-std::unordered_map<std::string, std::function<std::pair<llint, bool>()>> monkeys;
+std::unordered_map<std::string, std::function<std::string()>> monkeys;
 
 llint run(std::string const filename)
 {
@@ -39,12 +40,12 @@ llint run(std::string const filename)
         std::getline(ifs, line);
         if (std::regex_match(line, sm, pat_simp))
         {
-            llint val = stoi(sm[2]);
+            std::string val = sm[2];
             auto monkey_do = [val]()
             {
-                return std::pair(val, false);
+                return val;
             };
-            monkeys.insert({sm[1], std::function<std::pair<llint, bool>()> {monkey_do}});
+            monkeys.insert({sm[1], std::function<std::string()> {monkey_do}});
         } 
         else {
             std::regex_match(line, sm, pat);
@@ -59,84 +60,152 @@ llint run(std::string const filename)
             if (op == "*")
                 fun = std::multiplies<llint> {};
             
-            std::pair<llint, bool> res;
-            bool dirty = true;
-            auto monkey_do = [fun, lhs, rhs, res, dirty]() mutable
+            auto monkey_do = [op, lhs, rhs, fun]() mutable
             {
-                if(!dirty)
-                    return res;
                 auto lm = monkeys[lhs]();
                 auto rm = monkeys[rhs]();
-                dirty = lm.second || rm.second;
-                res = std::pair(fun(lm.first,rm.first), dirty);
-                return res;
+
+                ////todo division cant be switched
+                //if(rm.substr(0,2) == "(h")
+                //{
+                //    auto sw = lm;
+                //    lm = rm;
+                //    rm = sw;
+                //}
+
+                //if (lm == "humn")
+                //{
+                //    if (op == "+" || op == "-")
+                //        return "(humn " + op + rm + ")"; 
+                //}
+                //if (lm.substr(0,2) == "(h")
+                //{
+                //    std::regex pat_simp {R"((\(humn.*?) (.\d+)\))"};
+                //    std::smatch sm;
+                //    bool succ = std::regex_match(lm, sm, pat_simp);
+                //    std::cout<<"succ : "<<succ <<'\n';
+                //    if (op == "+" || op == "-")
+                //        return std::string(sm[1]) + " " +
+                //            std::to_string(
+                //                    fun(std::stoll(ss(sm[2])),
+                //                        std::stoll(rm))) + ")";
+                //    if (op == "*" || op == "*")
+                //    {
+                //        std::cout<<"ss(sm[2]: "<<ss(sm[2])<<'\n';
+                //        std::cout<<"ss(sm[3]): "<<ss(sm[3])<<'\n';
+                //        std::cout<<"std::stoll(ss(sm[2]): "<<std::stoll(ss(sm[2]) + ss(sm[3]))<<'\n';
+                //        std::cout<<"std::stoll(rm): "<<std::stoll(rm)<<'\n';
+                //        return std::string(sm[1])+op+rm + " " +
+                //            std::to_string(
+                //                    fun(std::stoll(ss(sm[2]) + ss(sm[3])),
+                //                        std::stoll(rm))) + ")";
+                //    }
+                //}
+                try
+                {
+                    return std::to_string(fun(std::stoll(lm), std::stoll(rm)));
+                } catch (std::invalid_argument const & e)
+                {
+                    ;
+                }
+                return "(" + lm + " " + op + " " + rm + ")";
             };
             if (sm[1] == "root")
             {
-                auto monkey_do2 = [fun, lhs, rhs, res, dirty]() mutable
+                auto monkey_do2 = [op, lhs, rhs]() mutable
                 {
-                    if(!dirty)
-                        return res;
                     auto lm = monkeys[lhs]();
                     auto rm = monkeys[rhs]();
-                    dirty = lm.second || rm.second;
-                    res = std::pair(lm.first - rm.first, dirty);
-                    return res;
+                    return lm + " " + "==" + " " + rm;
                 };
-                monkeys.insert({sm[1], std::function<std::pair<llint, bool>()> {monkey_do2}});
+                monkeys.insert({sm[1], std::function<std::string()> {monkey_do2}});
                 continue;
             }
-            monkeys.insert({sm[1], std::function<std::pair<llint, bool>()> {monkey_do}});
+            monkeys.insert({sm[1], std::function<std::string()> {monkey_do}});
         }
     }
 
-//230587548238637679 : res.first = 8;
-//230587548238637678 : res.first = 28;
-//230587548238637680 : res.first = -10;
-    llint max = std::numeric_limits<llint>::max();
-    llint test = max/2 - max/4;
-    auto monkey_do = [test]()
+    auto monkey_do = []()
     {
-        return std::pair(test, true);
+        return "humn";
     };
-    monkeys.insert_or_assign("humn", std::function<std::pair<llint,bool>()> {monkey_do});
+    monkeys.insert_or_assign("humn", std::function<std::string()> {monkey_do});
 
-    std::pair<llint, bool> res = monkeys["root"]();
-    while (res.first != 0)
+    std::string res = monkeys["root"]();
+    std::cout<<res<<'\n';
+    std::regex pat_res {R"((.*?) == (.*?))"};
+    std::smatch sm_res;
+    bool succ = std::regex_match(res, sm_res, pat_res);
+    std::cout<<"succ : "<<succ <<'\n';
+    std::string to_peel = sm_res[1];
+    std::cout<<"to_peel : "<<to_peel <<'\n';
+    llint rhs = stoll(sm_res[2]);
+    std::cout<<"rhs : "<<rhs <<'\n';
+    std::cout<<"to_peel.substr(-2,2) : "<<to_peel.substr(to_peel.size()-2,2) <<'\n';
+    std::regex pat_left {R"(\((\d+) (.) (.*))"};
+    std::regex pat_right {R"((.+?) (.) (\d+)\))"};
+    while (to_peel.substr(0,1) == "(")
     {
-        std::cout<<"res.first : "<<res.first <<'\n';
-        if (std::abs(res.first) < 10)
+        std::cout<<to_peel<<" == " << rhs<<'\n';
+        llint val;
+        std::string op;
+        if (std::regex_match(to_peel, sm, pat_left))
         {
-            ++test;
-        }
-        else
-        {
-            if(res.first < 0)
+            val = std::stoll(sm[1]);
+            op = sm[2];
+            to_peel = sm[3];
+            to_peel.pop_back();
+            if (op == "+")
             {
-                max = test;
-                test = test/2;
+                rhs -= val;
             }
-            std::cout<<"max : "<<max <<'\n';
-            std::cout<<"test : "<<test <<'\n';
-            if(res.first > 0)
-                test += (max-test)/2;
-        }
-        std::cout<<"test : "<<test <<'\n';
-        auto monkey_do = [test]()
-        {
-            return std::pair(test, true);
-        };
-        monkeys.insert_or_assign("humn", std::function<std::pair<llint,bool>()> {monkey_do});
-        res = monkeys["root"]();
-    }
+            if (op == "-")
+            {
+                rhs = -1 *(rhs-val);
+            }
+            if (op == "/")
+            {
+                rhs = val/rhs;
+            }
+            if (op == "*")
+            {
+                rhs = rhs/val;
+            }
 
-    return test;
+        } else
+        {
+            std::regex_match(to_peel, sm, pat_right);
+            val = std::stoll(sm[3]);
+            op = sm[2];
+            to_peel = sm[1];
+            to_peel = to_peel.substr(1);
+            if (op == "-")
+            {
+                rhs += val;
+            }
+            if (op == "/")
+            {
+                rhs *= val;
+            }
+            if (op == "+")
+            {
+                rhs -= val;
+            }
+            if (op == "*")
+            {
+                rhs /= val;
+            }
+        }
+
+    }
+    std::cout<<to_peel<<" == " << rhs<<'\n';
+    return 0;
 }
 
 int main(int argc, char** argv)
 {
     auto test_result = run("input_t1");
     std::cout<<"input_t1 result: "<<test_result<<'\n';
-    //auto result = run("input");
-    //std::cout<<"input result: "<<result<<'\n';
+    auto result = run("input");
+    std::cout<<"input result: "<<result<<'\n';
 }
