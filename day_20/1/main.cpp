05fabe6fc1d205bd966d30ad9ac688e54ba18006
dmad <dmad@t14g3.fritz.box>
2022-12-22 11:47:49 +0100

    Thanks to the Simon :)

--- day_20/1/main.cpp
+++ day_20/1/main.cpp
@@ -32,13 +32,7 @@ void pl(auto l)
 
 int get_insert_loc(int pos, int val, int lsize)
 {
-    //std::cout<<"(val % (int)lsize): "<<(val % (int)lsize)<<'\n';
-    //std::cout<<"(pos + (val % (int)lsize) + lsize): "<<(pos + (val % (int)lsize) + lsize)<<'\n';
-    //std::cout<<"(pos + (val % (int)lsize) + lsize) % (int)lsize;: "<<(pos + (val % (int)lsize) + lsize) % (int)lsize<<'\n';
     int t_i = (pos + (val % lsize) + lsize) % lsize;
-    if (val > 0)
-        t_i = (t_i + 1) % lsize;
-    //std::cout<<"t_i : "<<t_i <<'\n';
     return t_i;
 }
 
@@ -54,9 +48,9 @@ long long int run(std::string const filename)
         l.push_back({std::stoi(line), false});
     }
 
+    //pl(l);
     int moved = 0;
     int m_i = 0;
-    //pl(l);
     while (moved < (int)l.size())
     {
         if(l[m_i].second)
@@ -65,22 +59,14 @@ long long int run(std::string const filename)
             continue;
         }
         int val = l[m_i].first;
+        l.erase(l.begin() + m_i);
         int insert_i = get_insert_loc(m_i,val, (int)l.size());
         l.insert(l.begin() + insert_i, {{val, true}});
         if (insert_i <= m_i)
             ++m_i;
-        l.erase(l.begin() + m_i);
         ++moved;
         //pl(l);
     }
-
-    for(auto e : l)
-        if(!e.second)
-        {
-            std::cout<<"WTF";
-            abort();
-        }
-
 
     int zero_i = 0;
     for(int i = 0; i<(int)l.size(); ++i)
@@ -88,46 +74,13 @@ long long int run(std::string const filename)
             zero_i = i;
     std::cout<<"zero_i : "<<zero_i <<'\n';
 
-
-    std::cout<<"l[(zero_i+1000)%l.size()].first: "
-        <<l[(zero_i+1000)%(int)l.size()].first<<'\n';
-    std::cout<<"l[(zero_i+2000)%l.size()].first: "
-        <<l[(zero_i+2000)%(int)l.size()].first<<'\n';
-    std::cout<<"l[(zero_i+3000)%l.size()].first: "
-        <<l[(zero_i+3000)%(int)l.size()].first<<'\n';
     return l[(zero_i+1000)%(int)l.size()].first + l[(zero_i+2000)%(int)l.size()].first + l[(zero_i+3000)%(int)l.size()].first;
 }
 
 int main(int argc, char** argv)
 {
-    std::cout<<-7%(unsigned int)4<<'\n';
-    std::cout<<-7%(int)4<<'\n';
-    get_insert_loc(0,-5,4);
-    //abort();
-    int ls = 4;
-    for (int val = 0; val > -10; --val)
-    {
-        for(int pos = 0; pos<ls; ++pos)
-        {
-            std::cout<<get_insert_loc(pos,val,ls)<< " ";
-        }
-        std::cout<<'\n';
-    }
-    std::cout<<"\n\n";
-    for (int val = 0; val < 10; ++val)
-    {
-        for(int pos = 0; pos<ls; ++pos)
-        {
-            std::cout<<get_insert_loc(pos,val,ls)<< " ";
-        }
-        std::cout<<'\n';
-    }
-
-    auto test_result2 = run("input_t2");
-    std::cout<<"input_t2 result: "<<test_result2<<'\n';
     auto test_result = run("input_t1");
     std::cout<<"input_t1 result: "<<test_result<<'\n';
     auto result = run("input");
     std::cout<<"input result: "<<result<<'\n';
-    //9459
 }
