f0ce2bee58dd0e69267013a72a3da1a886cd8f9e
dmad <dmad@t14g3.fritz.box>
2022-12-24 16:59:57 +0100

    day_1 working

--- day_24/1/main.cpp
+++ day_24/1/main.cpp
@@ -26,6 +26,18 @@ template <typename T> int sgn(T val) {
 typedef std::vector<std::vector<std::vector<char>>> Map;
 typedef std::vector<std::vector<char>> Row;
 
+class valarray_comp
+{
+
+    public:
+        bool operator()(const std::valarray<int> & lhs, const std::valarray<int> & rhs) const
+        {
+            for (int i=0; i<(int)lhs.size(); ++i)
+                if (lhs[i] > rhs[i])
+                    return true;
+            return false;
+        };
+};
 std::vector<Map> maps;
 
 void pm(Map const & map)
@@ -38,10 +50,10 @@ void pm(Map const & map)
             switch (map[y][x].size())
             {
                 case 0:
-                    if(x==0 || y==0 ||
-                       x==(int)map[0].size()-1 || y==(int)map.size() -1)
-                        std::cout<<'#';
-                    else
+                    //if(x==0 || y==0 ||
+                    //   x==(int)map[0].size()-1 || y==(int)map.size() -1)
+                    //    std::cout<<'#';
+                    //else
                         std::cout<<'.';
                     break;
                 case 1:
@@ -56,7 +68,7 @@ void pm(Map const & map)
 }
 
 
-bool pat(int x, int y, int t)
+bool pat(std::valarray<int> pos)
 {
     static std::unordered_map<char,std::vector<int>> char2dir 
     {{'^',{0,-1}},
@@ -64,10 +76,22 @@ bool pat(int x, int y, int t)
      {'v',{0,1}},
      {'<',{-1,0}},
     };
-    while (t>(int)maps.size()-1)
+    while (pos[0]>(int)maps.size()-1)
     {
         Map & map = maps[maps.size()-1];
         Map next_t = Map(map.size(), Row(map[0].size(), std::vector<char>()));
+
+        for (int y : {0, (int)map.size() -1})
+            for (auto & col : next_t[y])
+                col.push_back('#');
+
+        for (auto & row : next_t)
+        {
+            row[0].push_back('#');
+            row[map[0].size()-1].push_back('#');
+        }
+                
+
         for (int y=1; y<(int)map.size()-1; ++y)
         {
             for (int x=1; x<(int)map[0].size()-1; ++x)
@@ -102,7 +126,11 @@ bool pat(int x, int y, int t)
         }
         maps.push_back(std::move(next_t));
     }
-    return maps[t][y][x].size() == 0;
+    //pm(maps[pos[0]]);
+    //std::cout<<"for pos:"<<pos[0]<<","<<pos[1]<<","<<pos[2]<<'\n';
+    //bool res = maps[pos[0]][pos[2]][pos[1]].size() == 0;
+    //std::cout<<"res : "<<res <<'\n';
+    return maps[pos[0]][pos[2]][pos[1]].size() == 0;
 }
 
 
@@ -126,7 +154,7 @@ luint run(std::string const filename)
             //next if is dumb but the last time we have a . is the exit
             if(line[x] == '.')
                 x_end = x;
-            if (line[x] != '.' && line[x] != '#')
+            if (line[x] != '.')// && line[x] != '#')
                 row.push_back(std::vector<char> {line[x]});
             else
                 row.push_back(std::vector<char> {});
@@ -138,14 +166,83 @@ luint run(std::string const filename)
     int y_end = map.size()-1;
     std::cout<<"start: ("<<x_start<<","<<y_start<<")\n";
     std::cout<<"end: ("<<x_end<<","<<y_end<<")\n";
+    map[y_start][x_start].push_back('#');
     maps.push_back(std::move(map));
+    pm(maps[maps.size()-1]);
+    //std::string blah;
+    //std::cin>>blah;
 
-    for (int i=0; i<3; ++i)
+
+    //for (int i=0; i<3; ++i)
+    //{
+    //    pat(std::valarray {i,1,1});
+    //    pm(maps[maps.size()-1]);
+    //}
+
+    auto comp = [](std::valarray<int> const & lhs,
+                                    std::valarray<int> const & rhs)
     {
-        pat(1,1,i);
-        pm(maps[maps.size()-1]);
-    }
+        //std::cout<<"("<<lhs[0]<<","<<lhs[1]<<","<<lhs[2]<<"), ";
+        //std::cout<<"("<<rhs[0]<<","<<rhs[1]<<","<<rhs[2]<<"), "<<'\n';
+        for (int i=0; i<(int)lhs.size(); ++i)
+        {
+            if (lhs[i] == rhs[i])
+                continue;
+            return lhs[i] < rhs[i];
+        }
+        return false;
+    };
+    std::set<std::valarray<int>, decltype(comp)> todo;
+    //todo.insert(std::valarray {1,4,1});
+    //todo.insert(std::valarray {1,3,1});
+    //todo.insert(std::valarray {2,3,2});
+    //todo.insert(std::valarray {3,2,3});
+    //todo.insert(std::valarray {4,1,4});
 
+    //std::for_each(todo.begin(), todo.end(), [](auto e)
+    //        {std::cout<<"("<<e[0]<<","<<e[1]<<","<<e[2]<<"), ";});
+    //std::cout<<std::endl;
+    //abort();
+
+
+
+    std::vector<std::valarray<int>> next_dir {
+        {1,  0,  1},
+        {1,  1,  0},
+        {1, -1,  0},
+        {1,  0,  0},
+        {1,  0, -1}
+    };
+    std::valarray<int> start_pos {0,x_start, y_start};
+    if (pat(start_pos+next_dir[0]))
+        todo.insert(start_pos+next_dir[0]);
+    else
+        todo.insert(start_pos+next_dir[3]);
+
+    while (todo.size() > 0)
+    {
+        //std::for_each(todo.begin(), todo.end(), [](auto e)
+        //        {std::cout<<e[0]<<", ";});
+        //std::cout<<'\n';
+        //std::cout<<'\n';
+        auto pos = *todo.begin();
+        //std::cout<<"for pos:"<<pos[0]<<","<<pos[1]<<","<<pos[2]<<'\n';
+        todo.erase(todo.begin());
+        for (auto dir : next_dir)
+        {
+            auto n_pos = pos + dir;
+            if (n_pos[1] == x_end && n_pos[2] == y_end)
+                return n_pos[0];
+            if (pat(n_pos))
+                todo.insert(n_pos);
+        }
+        //std::cout<<"todo.size() : "<<todo.size() <<'\n';
+        //for (auto e : todo)
+        //    std::cout<<"("<<e[0]<<","<<e[1]<<","<<e[2]<<"), ";
+        //std::cout<<'\n';
+        //std::string blah;
+        //std::cin>>blah;
+    }
     return 0;
 }
 
@@ -153,12 +250,15 @@ int main(int argc, char** argv)
 {
     auto start = std::chrono::steady_clock::now();
 
-    auto test_result = run("input_t1");
-    std::cout<<"input_t1 result: "<<test_result<<'\n';
+    auto test_result1 = run("input_t1");
+    std::cout<<"input_t1 result: "<<test_result1<<'\n';
+    auto test_result2 = run("input_t2");
+    std::cout<<"input_t2 result: "<<test_result2<<'\n';
     auto result = run("input");
     std::cout<<"input result: "<<result<<'\n';
 
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> elapsed_seconds = end-start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+    //568 too high
 }
