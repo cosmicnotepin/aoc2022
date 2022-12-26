9dc7477f93161c79cc320017e6e79b048cbc6ab5
dmad <dmad@t14g3.fritz.box>
2022-12-26 14:54:43 +0100

    day2/2 bettah

--- day_2/1/main.cpp
+++ day_2/1/main.cpp
@@ -4,20 +4,24 @@
 int score(std::string filename)
 {
     std::ifstream ifs {filename};
-    int final_score = 0;
+    int score_p1 = 0;
+    int score_p2 = 0;
     for (std::string s; getline(ifs, s);)
     {
         char op = s[0] - 'A';
         char me = s[2] - 'X';
-        final_score += ((me-op+4)%3)*3 + me + 1;
+        score_p1 += ((me-op+4)%3)*3 + me + 1;
+        score_p2 += ((op + me + 5)%3) + 1 + me*3;
     }
-    return final_score;
+    std::cout<<"p1: "<<score_p1<<'\n';
+    std::cout<<"p2: "<<score_p2<<'\n';
+    return 0;
 }
 
 int main(int argc, char** argv)
 {
-    std::cout<<"t1: "<<score("input_t1")<<'\n';
-    std::cout<<"p1: "<<score("input")<<'\n';
+    score("input_t1");
+    score("input");
 }
 
 
--- day_2/1/p.py
+++ day_2/1/p.py
@@ -1,16 +1,18 @@
 def  main(filename):
-    score = 0
+    score_p1 = 0
+    score_p2 = 0
     for line in open(filename, "r").read().splitlines():
         [op, me] = line.split(" ");
         op = ord(op) - ord('A')
         me = ord(me) - ord('X')
-        score += ((me-op+1) % 3) * 3 + me + 1
+        score_p1 += ((me-op+1) % 3) * 3 + me + 1
+        score_p2 += (op + me - 1) % 3 + 1 + me * 3
 
-    return score
+    return score_p1, score_p2
 
 
 if __name__ == "__main__":
-    res_t = main("input_t1")
-    print(f't1: {res_t }')
-    res_t = main("input")
-    print(f'p1: {res_t }')
+    res_t1, res_t2= main("input_t1")
+    print(f't1: {res_t1}, t2: {res_t2}')
+    res_t1, res_t2 = main("input")
+    print(f'p1: {res_t1}, p2: {res_t2}')
