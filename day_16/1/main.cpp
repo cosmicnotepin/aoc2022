1bc6e00240fc1f7a7453d892279ac10bde20f3ab
dmad <dmad@t14g3.fritz.box>
2022-12-29 18:10:40 +0100

    day_16/1 first wv of bettah

--- day_16/1/main.cpp
+++ day_16/1/main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
 #include <unordered_map>
 #include <fstream>
 #include <vector>
 #include <regex>
-#include <stack>
-#include <queue>
-#include <sstream>
-#include <list>
 #include <string>
-#include <memory>
-#include <set>
-#include <functional>
-#include <limits>
-#include <algorithm>
+#include <unordered_set>
+#include <chrono>
 
 typedef long unsigned int luint;
 
@@ -22,45 +13,25 @@ template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-luint dfs(std::string & target,
-        std::map<std::string, bool> & vss,
-        std::map<std::string, luint> & fs,
-        std::map<std::string, std::vector<std::string>> & cs,
-        luint time,
-        luint res,
-        luint current_fr,
-        std::map< std::string, std::map< luint, luint>> & brsf,
-        std::map< std::string, std::map< luint, luint>> & bfrsf)
-{
-    if (time < 2)
-        return res;
-    //if we have been here before with better flowrate 
-    if (brsf[target][time] > res )//&& bfrsf[target][time] > current_fr) 
-        return 0;
-    //if (brsf[target][time] > res && bfrsf[target][time] > current_fr) 
-    //    return 0;
-    brsf[target][time] = res;
-    bfrsf[target][time] = current_fr;
+std::unordered_map<std::string, int> fs;
+std::unordered_map<std::string, std::vector<std::string>> neighss;
+std::unordered_map<std::string, std::unordered_map<std::string, int>> dists;
 
-    luint max = 0;
-    //first check without opening this valve
-    for (std::string & c : cs[target])
-    {
-        max = std::max(max, dfs(c,vss,fs,cs,time-1,res, current_fr, brsf, bfrsf));
-    }
-    
-    //then check with opening this valve
-    if (vss[target] == false && fs[target] > 0)
-    {
-        vss[target] = true;
-        current_fr += fs[target];
-        time -= 1;
-        res += time * fs[target];
-        for (std::string & c : cs[target])
-        {
-            max = std::max(max, dfs(c,vss,fs,cs,time-1,res, current_fr, brsf, bfrsf));
+
+int dfs(std::string const & cr, std::unordered_map<std::string, bool> vss,
+        int time, int fsf, std::unordered_set<std::string> visited)
+{
+    if (time <= 0)
+        return 0;
+    vss[cr] = true;
+    fsf += time * fs[cr];
+    int max = fsf;
+    for (auto n : dists[cr]) {
+        if (!visited.contains(n.first)) {
+            std::unordered_set<std::string> vc(visited); 
+            vc.insert(n.first);
+            max = std::max(max, dfs(n.first, vss, time - n.second - 1, fsf, vc));
         }
-        vss[target] = false;
     }
     return max;
 }
@@ -78,46 +49,72 @@ luint run(std::string const filename)
     std::smatch sm;
     std::regex pat2 {R"((\S\S),?)"};
     std::smatch sm2;
-    std::map<std::string, std::vector<std::string>> cs;
-    std::map<std::string, luint> fs;
-    std::map< std::string, std::map< luint, luint>> brsf;
-    std::map< std::string, std::map< luint, luint>> bfrsf;
-    std::map<std::string, bool> vss;
+    std::unordered_map<std::string, bool> vss;
 
-    while (ifs.peek() != EOF)
-    {
+    fs.clear();
+    neighss.clear();
+    dists.clear();
+    while (ifs.peek() != EOF) {
         std::vector<std::vector<luint>> rocks;
         std::string line;
         std::getline(ifs, line);
         std::regex_match(line, sm, pat);
-        //std::cout<<"sm[1]: "<<sm[0]<<'\n';
         std::string valvename = sm[1];
-        ////std::cout<<"valvename : "<<valvename <<'\n';
-        vss.insert({valvename, false});
-        brsf.insert({valvename, {}});
-        bfrsf.insert({valvename, {}});
-        luint flowrate = stoul(sm[2]);
-        //std::cout<<"flowrate : "<<flowrate <<'\n';
-        fs.insert({valvename, flowrate});
-        std::string connections = sm[3];
-        //std::cout<<"connections : "<<connections <<'\n';
-        std::vector<std::string> conns;
-        for (auto conn = std::sregex_iterator(connections.begin(), connections.end(), pat2);
-                conn != std::sregex_iterator(); ++conn) {
-            //std::cout<<"    conn would be: "<<(*conn)[0]<<"\n";
-            conns.push_back((*conn)[1]);
+        fs.insert({valvename, stoi(sm[2])});
+
+        std::string neigh_string = sm[3];
+        std::vector<std::string> neighs;
+        for (auto n = std::sregex_iterator(neigh_string.begin(), neigh_string.end(), pat2);
+                n != std::sregex_iterator(); ++n) {
+            neighs.push_back((*n)[1]);
         }
-        cs.insert({valvename, conns});
+        neighss.insert({valvename, neighs});
     }
+
+    //for (auto fr : fs)
+    //    std::cout<<fr.first<<":"<<fr.second<<"\n";
+
+    for (auto v : fs) {
+        if (v.first != "AA" && v.second == 0)
+            continue;
+        std::deque<std::pair<std::string, int>> todo;
+        std::unordered_set<std::string> visited;
+        todo.push_back({v.first, 0});
+        while (todo.size() > 0) {
+            auto cr = todo.front();
+            todo.pop_front();
+            if (fs[cr.first] > 0 && v.first != cr.first)
+                dists[v.first][cr.first] = cr.second;
+            for (auto n: neighss[cr.first]) {
+                if (!visited.contains(n)) {
+                    visited.insert(n);
+                    todo.push_back({n, cr.second + 1});
+                }
+            }
+        }
+    }
+
+    //for (auto ds : dists) {
+    //    std::cout<<ds.first<<": [";
+    //    for (auto d : ds.second)
+    //        std::cout<<d.first<<":"<<d.second<<", ";
+    //    std::cout<<"]\n";
+    //}
+
     std::string start_pos = "AA";
-    luint res = dfs(start_pos,vss,fs,cs,30,0,0,brsf,bfrsf);
+    std::unordered_set<std::string> visited;
+    int res = dfs(start_pos,vss,30,0,visited);
     return res;
 }
 
 int main(int argc, char** argv)
 {
+    auto start = std::chrono::steady_clock::now();
     auto test_result = run("input_t1");
     std::cout<<"input_t1 result: "<<test_result<<'\n';
     auto result = run("input");
     std::cout<<"input result: "<<result<<'\n';
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 }
