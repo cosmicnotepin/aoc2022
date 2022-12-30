301f96d9ce1fc203576bc87af4cf477d8d6dc35e
dmad <dmad@t14g3.fritz.box>
2022-12-30 07:49:16 +0100

    day_16/2 first cleanup of old, not yet broken

--- day_16/2/main.cpp
+++ day_16/2/main.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
 #include <unordered_map>
 #include <fstream>
 #include <vector>
 #include <regex>
-#include <stack>
-#include <queue>
 #include <sstream>
-#include <list>
 #include <string>
-#include <memory>
 #include <set>
-#include <functional>
 #include <limits>
-#include <algorithm>
 
 typedef long unsigned int luint;
 
-std::map<std::string, size_t> name2index;
-std::map<size_t, std::string> index2name;
-std::vector<luint> vfs;
+std::unordered_map<std::string, int> name2index;
+std::vector<int> fs;
 std::vector<bool> vvss;
-std::vector<std::vector<std::vector<luint>>> vbrsf;
-std::vector<std::vector<luint>> vcs;
-luint full_time = 26;
+std::vector<std::vector<std::vector<int>>> vbrsf;
+std::vector<std::vector<int>> vcs;
+int full_time = 26;
 
 template <typename T> int sgn(T val) {
     return (T(0) < val) - (val < T(0));
 }
 
-luint dfs(luint target, luint etarget, luint time, luint res)
+int dfs(int target, int etarget, int time, int res)
 {
     if (time == 0)
         return res;
 
-    for (size_t i = full_time; i >= time; --i)
+    for (int i = full_time; i >= time; --i)
     {
         if (vbrsf[i][target][etarget] != 0 && vbrsf[i][target][etarget] >= res) 
             return 0;
@@ -44,41 +35,39 @@ luint dfs(luint target, luint etarget, luint time, luint res)
     vbrsf[time][target][etarget] = res;
     vbrsf[time][etarget][target] = res;
 
-    luint max = 0;
+    int max = 0;
 
-    if ( target != etarget && !vvss[target] && vfs[target] > 0 && !vvss[etarget] && vfs[etarget] > 0)
+    if ( target != etarget && !vvss[target] && fs[target] > 0 && !vvss[etarget] && fs[etarget] > 0)
     {
         vvss[target] = true;
         vvss[etarget] = true;
-        luint tres = res + (time-1) * (vfs[target] + vfs[etarget]);
+        int tres = res + (time-1) * (fs[target] + fs[etarget]);
         max = std::max(max, dfs(target,etarget,time-1,tres));
         vvss[target] = false;
         vvss[etarget] = false;
     }
 
-    if (!vvss[target] && vfs[target] > 0 && !(!vvss[etarget] && vfs[etarget] > 0))
+    if (!vvss[target] && fs[target] > 0 && !(!vvss[etarget] && fs[etarget] > 0))
     {
         vvss[target] = true;
-        luint tres = res + (time-1) * vfs[target];
-        for (luint & ec : vcs[etarget])
+        int tres = res + (time-1) * fs[target];
+        for (int & ec : vcs[etarget])
             max = std::max(max, dfs(target,ec,time-1,tres));
         vvss[target] = false;
     }
 
-    if (!(!vvss[target] && vfs[target] > 0) && !vvss[etarget] && vfs[etarget] > 0)
+    if (!(!vvss[target] && fs[target] > 0) && !vvss[etarget] && fs[etarget] > 0)
     {
         vvss[etarget] = true;
-        luint tres = res + (time-1) * (vfs[etarget]);
-        for (luint & c : vcs[target])
+        int tres = res + (time-1) * (fs[etarget]);
+        for (int & c : vcs[target])
             max = std::max(max, dfs(c,etarget,time-1,tres));
         vvss[etarget] = false;
     }
 
     //both move without activating valves
-    for (luint & c : vcs[target])
-    {
-        for (luint & ec : vcs[etarget])
-        {
+    for (int & c : vcs[target]) {
+        for (int & ec : vcs[etarget]) {
             if (c != etarget && ec != target)
                 max = std::max(max, dfs(c,ec,time-1,res));
         }
@@ -87,83 +76,49 @@ luint dfs(luint target, luint etarget, luint time, luint res)
     return max;
 }
 
-luint run(std::string const filename)
+int run(std::string const filename)
 {
     std::ifstream ifs {filename};
-    if(!ifs)
-    {
-        std::cerr<<"could not open "<<filename<<'\n';
-        exit(1);
-    }
-
     std::regex pat {R"(Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.*))"};
     std::smatch sm;
     std::regex pat2 {R"((\S\S),?)"};
     std::smatch sm2;
-    std::map<std::string, std::vector<std::string>> cs;
-    std::map<std::string, luint> fs;
-    std::map< std::string, std::map< luint, luint>> brsf;
-    std::map<std::string, bool> vss;
+    std::vector<std::vector<std::string>> cs;
 
-    while (ifs.peek() != EOF)
-    {
-        std::vector<std::vector<luint>> rocks;
-        std::string line;
-        std::getline(ifs, line);
+    for (std::string line; std::getline(ifs, line);) {
         std::regex_match(line, sm, pat);
         std::string valvename = sm[1];
-        vss.insert({valvename, false});
-        brsf.insert({valvename, {}});
-        luint flowrate = stoul(sm[2]);
-        fs.insert({valvename, flowrate});
+        vvss.push_back(false);
+        name2index.insert({valvename, name2index.size()});
+        fs.push_back(stoi(sm[2]));
         std::string connections = sm[3];
         std::vector<std::string> conns;
         for (auto conn = std::sregex_iterator(connections.begin(), connections.end(), pat2);
                 conn != std::sregex_iterator(); ++conn) {
             conns.push_back((*conn)[1]);
         }
-        cs.insert({valvename, conns});
+        cs.push_back(conns);
     }
 
-
-    name2index.clear();
-    index2name.clear();
-    vfs.clear();
-    vvss.clear();
-    vbrsf.clear();
-    vcs.clear();
-
-    size_t i = 0;
-    for (auto val : vss)
+    for (size_t i = 0; i<fs.size(); ++i)
     {
-        name2index.insert({val.first, i});
-        index2name.insert({i++, val.first});
         vcs.push_back({});
-        vfs.push_back({});
-        vvss.push_back(val.second);
+        for (auto c : cs[i])
+            vcs[i].push_back(name2index[c]);
     }
 
-    for (auto val : fs)
-    {
-        vfs[name2index[val.first]] = val.second;
-    }
 
-    for (size_t j = 0; j<full_time+1; ++j)
+    for (int j = 0; j<full_time+1; ++j)
     {
-        std::vector<std::vector<luint>> time;
+        std::vector<std::vector<int>> time;
         for (size_t i = 0; i<name2index.size(); ++i)
-            time.push_back(std::vector<luint>(name2index.size()));
+            time.push_back(std::vector<int>(name2index.size()));
         vbrsf.push_back(time);
     }
 
-    for (auto val : cs)
-    {
-        for (auto conn : val.second)
-            vcs[name2index[val.first]].push_back(name2index[conn]);
-    }
 
-    luint start_pos = name2index["AA"];
-    luint res = dfs(start_pos,start_pos,full_time,0);
+    int start_pos = name2index["AA"];
+    int res = dfs(start_pos,start_pos,full_time,0);
     return res;
 }
 
@@ -171,6 +126,11 @@ int main(int argc, char** argv)
 {
     auto test_result = run("input_t1");
     std::cout<<"input_t1 result: "<<test_result<<'\n';
+    fs.clear();
+    name2index.clear();
+    vvss.clear();
+    vcs.clear();
+    vbrsf.clear();
     auto result = run("input");
     std::cout<<"input result: "<<result<<'\n';
 }
